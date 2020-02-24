d83bf4af46f230e4398d09be42071147152bc751
grecia.27 <greciarfr@hotmail.com>
2020-02-24 12:21:04 -0500

    avance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import validator from './validator.js';
 
-console.log(validator);
+const contenedor2 = document.getElementById('contenedor3');
+const contenedor3 = document.getElementById('contenedor2');
+contenedor2.style.display = 'none';
+
+function siguiente (){
+  const siguiente=documento.getElementById("verify").value;
+  const resp= documento.getElementById("contenedor3");
+  contenedor3.style.display = 'block';
+  contenedor2.style.display = 'none';
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,21 @@
-const validator = {
-  // ...
-};
+const numerotarjeta2 = ("4137894711755904");
+let reverse = Array.from(numerotarjeta2); //array.from= crea una nueva instancia de Array
+// me separa en digitos los numeros en string (pone comas a cada elemento del str)
+console.log(reverse);
+// paso los digitos str a digitos numeros
+let finalreverse = []; // array vacio para crear mi nuevo array
+// recorro los elementos de mi array para que sean transformados en numeros
+for (let i = 0; i < reverse.length; i++) {
+      finalreverse.push(parseInt(reverse[i]));
+      // a cada valor de i lo transformo en numero, y lo pongo a finalreverse
+      console.log(finalreverse);
+      }
+//se reversa el #
+let reverse2 = finalreverse.reverse();
+   console.log(reverse2) ; // [4095571174987314]
 
-export default validator;
+//  ubico los numeros en posicion par
+  for (let i = 0 ; i < reverse2.length; i += 2) {
+  console.log(reverse2);
+//se multiplan por 2
+    let doubleNum = reverse2[i] * 2;
