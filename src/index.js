064200bd6da3f1223beaca799a4984e957e826a9
grecia.27 <greciarfr@hotmail.com>
2020-02-27 09:06:31 -0500

    pasando a visual

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+import validator from './validator.js';
+
 // llamamos
-const contenedor2 = document.getElementById('contenedor3');
-const contenedor3 = document.getElementById('contenedor2');
-contenedor2.style.display = 'none';
-//crear funcion para ocultar pagina 2 de la 1
-function siguiente (){
-  const siguiente=documento.getElementById("verify").value;
-  const resp= documento.getElementById("contenedor3");
-  contenedor3.style.display = 'block';
-  contenedor2.style.display = 'none';
-}
+const contenedor2=document.getElementById('contenedor2');
+const nombrecompleto= document.getElementById('nombrecompleto');
+const numcreditcard=document.getElementById('numcreditcard');
+const opcion1=document.getElementById('opcion1');
+const opcion2=document.getElementById('opcion2');
+const verify= document.getElementById('verify');
+const contenedor3= document.getElementById('contenedor3');
+const messageFinal=document.getElementById('messageFinal');
+const result=document.getElementById('result');
+
+
+//pagina 3 resultado
+document.getElementById('verify').addEventListener('click',() =>{
+
+  let
+})
